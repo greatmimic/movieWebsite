65ab4f482552ebf48a526c4d44121e28bcc2bbbc
greatmimic <tjdgns1256@gmail.com>
2023-11-11 15:40:05 -0800

    Update mainPage.js

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -10,7 +10,7 @@ import MovieCard from './movieCard';
 
 const API_KEY = process.env.REACT_APP_API_KEY
 
-const API_URL = `http://www.omdbapi.com?apikey=${API_KEY}`;
+const API_URL = `https://www.omdbapi.com?apikey=${API_KEY}`;
 
 
 export default function MainPage() {
